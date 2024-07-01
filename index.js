c0eabb6049e9f8504b488a7f413a2c4732e2cb3c
oduba samuel <samueloduba123@gmail.com>
2024-07-01 11:12:07 +0100

    changed port number

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 3000;
+
 
 require("dotenv").config();
+const port = process.env.PORT;
+
 
 
 
